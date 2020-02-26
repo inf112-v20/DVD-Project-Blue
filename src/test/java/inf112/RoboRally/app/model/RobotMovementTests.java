0d7d489d77b76890c9542037c3042e80e1ca6447
Henrik Lander <henrik.lander@live.no>
2020-02-26 18:36:14 +0100

    Added tests for robot movement

---
+++ src/test/java/inf112/RoboRally/app/model/RobotMovementTests.java
@@ -0,0 +1,108 @@
+package inf112.RoboRally.app.model;
+
+import inf112.RoboRally.app.models.board.Direction;
+import inf112.RoboRally.app.models.board.Position;
+import inf112.RoboRally.app.models.board.Robot;
+import org.junit.Before;
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+
+public class RobotMovementTests {
+
+    private Robot robot;
+
+    @Before
+    public void setUp() {
+        robot = new Robot(new Position(0, 2), Direction.RIGHT);
+    }
+
+    @Test
+    public void testPositionWhenInitialized() {
+        assertEquals(0, robot.getX());
+        assertEquals(2, robot.getY());
+    }
+
+    @Test
+    public void testMovementByZero() {
+        robot.move(0);
+        assertEquals(0, robot.getX());
+    }
+
+    @Test
+    public void testMovementInDirection() {
+        robot.move(3);
+        assertEquals(3, robot.getX());
+    }
+
+    @Test
+    public void testConsecutiveMovementsInDirection() {
+        robot.move(7);
+        assertEquals(7, robot.getX());
+        robot.move(3);
+        assertEquals(10, robot.getX());
+    }
+
+    @Test
+    public void testBigMovement() {
+        robot.move(100000);
+        assertEquals(100000, robot.getX());
+        robot.move(5);
+        assertEquals(100005, robot.getX());
+    }
+
+    @Test
+    public void testNegativeMovement() {
+        robot.move(-5);
+        assertEquals(-5, robot.getX());
+    }
+
+    @Test
+    public void testDirectionWhenInitialized() {
+        assertEquals(Direction.RIGHT, robot.getDirection());
+    }
+
+    @Test
+    public void testChangeInDirection() {
+        robot.rotateDirectionLeft();
+        assertEquals(Direction.UP, robot.getDirection());
+    }
+
+    @Test
+    public void testChangeInDirectionBackAndForth() {
+        robot.rotateDirectionLeft();
+        robot.rotateDirectionLeft();
+        robot.rotateDirectionLeft();
+        robot.rotateDirectionRight();
+        assertEquals(Direction.LEFT, robot.getDirection());
+    }
+
+    @Test
+    public void testMovementAfterChangeInDirection() {
+        robot.rotateDirectionLeft();
+        robot.move(4);
+        assertEquals(6, robot.getY());
+    }
+
+    @Test
+    public void testNegativeMovementAfterChangeInDirection() {
+        robot.rotateDirectionLeft();
+        robot.move(-5);
+        assertEquals(-3, robot.getY());
+    }
+
+    @Test
+    public void testMultipleDirectionChangesAndMovements() {
+        robot.rotateDirectionLeft();
+        robot.move(-1);
+        robot.rotateDirectionRight();
+        robot.move(3);
+        robot.rotateDirectionRight();
+        robot.move(4);
+        robot.rotateDirectionRight();
+        robot.move(4);
+        assertEquals(-1, robot.getX());
+        assertEquals(-3, robot.getY());
+    }
+
+}
