600ea9260518e5954f97dba3a69701eec6573bc9
Gytis Byckovas <gytis.byckovas@gmail.com>
2020-02-27 14:36:48 +0100

    Few small changes to main and updated lwjgl

--- src/main/java/inf112/RoboRally/app/Main.java
+++ src/main/java/inf112/RoboRally/app/Main.java
@@ -10,10 +10,12 @@ public class Main {
     public static final int SCREEN_HEIGHT = LwjglApplicationConfiguration.getDesktopDisplayMode().height;
 
     public static void main(String[] args) {
+        System.setProperty("org.lwjgl.opengl.Display.enableHighDPI", "true");
         config = new LwjglApplicationConfiguration();
         config.title = "Robo Rally";
-        config.width = 1366;
-        config.height = 768;
+        config.width = SCREEN_WIDTH-267;
+        config.height = SCREEN_HEIGHT-150;
+        config.useHDPI = true;
         config.fullscreen = false;
         config.resizable = false;
         config.backgroundFPS = 60;
