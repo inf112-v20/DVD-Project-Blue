1df3df1cf5fa84fddf2f3b07cc72a281eebbac24
Gytis Byckovas <gytis.byckovas@gmail.com>
2020-02-29 09:22:28 +0100

    Generates random cards when index and priority is given

--- src/main/java/inf112/RoboRally/app/views/cards/Card.java
+++ src/main/java/inf112/RoboRally/app/views/cards/Card.java
@@ -7,19 +7,35 @@ import com.badlogic.gdx.scenes.scene2d.ui.Label;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 
-import java.awt.*;
-
 public class Card {
 
     final private Skin skin = new Skin(Gdx.files.internal("ButtonSkin/button-ui.json"));
 
-    public Group move1 (int priorityNum) {
+    public Group init (int index, int priorityNum) {
+        String texturePath = new String("Cards/");
+
+        if (index == 0){
+            texturePath += "Move1.png";
+        } else if (index == 1) {
+            texturePath += "Move2.png";
+        } else if (index == 2) {
+            texturePath += "Move3.png";
+        } else if (index == 3) {
+            texturePath += "BackUp.png";
+        } else if (index == 4) {
+            texturePath += "RotateLeft.png";
+        } else if (index == 5) {
+            texturePath += "RotateRight.png";
+        } else {
+            texturePath += "U-Turn.png";
+        }
+
         Group move1Group = new Group();
 
         Label priority = new Label(String.format("%04d", priorityNum), skin);
         priority.setFontScale(1/2.5f);
         priority.setPosition(101, 191);
-        Texture move1Texture = new Texture("Cards/Move1.png");
+        Texture move1Texture = new Texture(texturePath);
         move1Texture.setFilter(Texture.TextureFilter.Linear, Texture.TextureFilter.Linear);
         Image move1 = new Image(move1Texture);
         move1.setScale(1/4f);
--- src/main/java/inf112/RoboRally/app/views/cards/CardUI.java
+++ src/main/java/inf112/RoboRally/app/views/cards/CardUI.java
@@ -3,9 +3,12 @@ package inf112.RoboRally.app.views.cards;
 import com.badlogic.gdx.scenes.scene2d.InputEvent;
 import com.badlogic.gdx.scenes.scene2d.ui.Table;
 import com.badlogic.gdx.scenes.scene2d.utils.DragAndDrop;
-import com.badlogic.gdx.utils.Align;
+
+import java.util.Random;
 
 public class CardUI {
+
+    private Random random;
 
     public Card card, card2, card3, card4, card5, card6,
             card7, card8, card9;
@@ -14,6 +17,8 @@ public class CardUI {
         Table cards = new Table();
         cards.center().padRight(190);
         cards.setFillParent(true);
+
+        random = new Random();
 
         card = new Card();
         card2 = new Card();
@@ -25,23 +30,23 @@ public class CardUI {
         card8 = new Card();
         card9 = new Card();
 
-        cards.add(card.move1(1));
-        cards.add(card2.move1(2)).padLeft(200);
-        cards.add(card3.move1(3)).padLeft(200);
-        cards.add(card4.move1(4)).padLeft(200);
-        cards.add(card5.move1(5)).padLeft(200);
-        cards.add(card6.move1(6)).padLeft(200);
-        cards.add(card7.move1(7)).padLeft(200);
-        cards.add(card8.move1(8)).padLeft(200);
-        cards.add(card9.move1(9)).padLeft(200);
+        cards.add(card.init(0, 1));
+        cards.add(card2.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card3.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card4.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card5.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card6.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card7.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card8.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
+        cards.add(card9.init(random.nextInt(6), random.nextInt(1999))).padLeft(200);
 
         DragAndDrop dnd = new DragAndDrop();
         dnd.addSource(new DragAndDrop.Source(cards) {
             final DragAndDrop.Payload payload = new DragAndDrop.Payload();
             @Override
             public DragAndDrop.Payload dragStart(InputEvent event, float x, float y, int pointer) {
-                payload.setObject(card.move1(1));
-                payload.setDragActor(card.move1(1));
+                payload.setObject(card.init(0, 1));
+                payload.setDragActor(card.init(0, 1));
                 return payload;
             }
         });
