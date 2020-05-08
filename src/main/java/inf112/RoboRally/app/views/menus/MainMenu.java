d9fd1864643e70329f18fcd3063a1d5aad2e6d5d
Henrik Lander <henrik.lander@live.no>
2020-05-08 15:17:17 +0200

    menu button name change

--- src/main/java/inf112/RoboRally/app/views/menus/MainMenu.java
+++ src/main/java/inf112/RoboRally/app/views/menus/MainMenu.java
@@ -46,7 +46,7 @@ public class MainMenu implements Screen {
 
         TextButton play = new Button().createTextButton("PLAY");
         TextButton lan = new Button().createTextButton("LAN");
-        TextButton testGame = new Button().createTextButton("WALL TEST");
+        TextButton testGame = new Button().createTextButton("PLAYGROUND");
         TextButton quit = new Button().createTextButton("QUIT");
         table.add(play);
         table.row().padTop(10);
